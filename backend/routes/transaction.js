c72f629dc9c947a2769f89d82bb13c1f1a09b945
NIKHIL GUPTA <nikhil20530@iiitd.ac.in>
2023-11-03 20:21:07 +0530

    Transaction controller completed (not tested)

---
+++ backend/routes/transaction.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const { createRequest, acceptRequest, declineRequest, confirmTransaction, getAllRequests } = require('../controllers/allocationController');
+
+// Create a new equipment request (Student)
+router.post('/requests', createRequest);
+
+// Accept a request (Admin)
+router.put('/requests/accept/:transactionId', acceptRequest);
+
+// Decline a request (Admin)
+router.put('/requests/decline/:transactionId', declineRequest);
+
+// Confirm a transaction (Admin)
+router.put('/transactions/confirm/:transactionId', confirmTransaction);
+
+// Get all requests (Admin)
+router.get('/requests', getAllRequests);
+
+module.exports = router;
